7a17cf135a8ac6905c86e8196409540c8dff29d3
esalinas00 <efsalinas00@gmail.com>
2016-03-20 21:02:17 +0800

    Solve learnyounode/first-async-io

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,6 +1,9 @@
-var fs = require('fs'),
-  path = process.argv[2],
-  lines;
+var fs = require('fs');
+var path = process.argv[2];
+var countLines;
 
-lines = fs.readFileSync(path,'utf8').split('\n').length - 1;
-console.log(lines)
+fs.readFile(path,function cb(err, data){
+  if (err) console.error(err);
+  countLines = data.toString().split('\n').length - 1
+  console.log(countLines);
+});
